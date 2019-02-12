8b39e2a5c871eaf417ea07728e3841a7c84f9e4b
saikishunsuke <saikishunsuke0828@yahoo.co.jp>
2019-02-12 17:47:49 +0900

    .gitignore is now working

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='shop/')),
     path('admin/', admin.site.urls),
     path('shop/', include('shop.urls')),
     path('board/', include('board.urls')),
